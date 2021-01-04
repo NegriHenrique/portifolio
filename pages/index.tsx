089d7600a97782b1a273a550d399e26e8ce99952
Kayo ELias <contatodevkayo@gmail.com>
2021-01-04 19:06:08 -0300

    Index adicionada ao projeto

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const pages: React.FC = () => {
+  return (
+      <div>
+          <h1>Hi Love = ) </h1>
+      </div>
+  );
+}
+
+export default pages;
